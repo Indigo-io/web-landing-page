108ec5c9195cba2485866418ecd53854b3d9384b
Exequiel Demaio <exequiel.demaio@gmail.com>
2021-10-20 20:04:52 -0300

    add calendar

--- components/Wizard/Step10.js
+++ components/Wizard/Step10.js
@@ -1,3 +1,5 @@
+import { InlineWidget } from "react-calendly";
+
 export default function Step10() {
   return (
     <div className="wizard-step">
@@ -6,9 +8,19 @@ export default function Step10() {
           Gracias por contestar! Ahora ya te conocemos un poquito más.
         </h2>
         <p className="text-center fs-5">
-          Muy pronto te estará llegando un mail con novedades. Acordate de
-          chequear la carpeta de spam!
+          Agenda tu cita con expertos para continuar este cambio que ya empezó.
         </p>
+
+        <p className="text-center" style={{ fontSize: "16px" }}>
+          En caso que ahora no puedas te enviamos un mail
+        </p>
+
+        <InlineWidget
+          url="https://calendly.com/indigoexperts/30min"
+          styles={{
+            height: "1070px",
+          }}
+        />
       </div>
     </div>
   );
