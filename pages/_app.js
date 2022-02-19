695955bd3f7151ebf03519421b644f757ec16876
Exequiel Demaio <exequiel.demaio@gmail.com>
2022-02-19 03:04:06 -0300

    new design

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,14 @@
 import "../styles/globals.css";
+import "../styles/hero.css";
+import "../styles/video.css";
+import "../styles/google-play.css";
+import "../styles/footer.css";
+import "../styles/camino.css";
+import "../styles/testimonials.css";
+import "../styles/contact.css";
+import "../styles/start-now.css";
+import "../styles/sub-hero.css";
+import "../styles/prices.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
