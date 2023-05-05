e94d4c132c9fec066fb1708393712295f01f1e38
Rodrigo Avila <rodrigoavila@MacBook-Pro-de-Rodrigo.local>
2023-05-05 16:18:57 -0300

    fix conflict

--- components/GooglePlay.js
+++ components/GooglePlay.js
@@ -1,17 +1,13 @@
 import React from "react";
-<<<<<<< HEAD
 
-=======
-import Link from "next/link";
->>>>>>> aee4788c2ecff0effaa5b499c346670d148f8fe9
+
+
 
 export default function GooglePlay() {
   return (
     <div className="google-play">
       <div className="container">
         <div className="google-play__wrapper">
-<<<<<<< HEAD
-
 
         <div class="left-half">
             <h1 className="google-play__title">
@@ -46,42 +42,6 @@ export default function GooglePlay() {
             </div>
           
           </div>
-
-=======
-          <div className="google-play__wrapper--text">
-            <h3 className="google-play__title">
-              Comienza a incorporar tu primer hábito. Pon tu inconsciente a tu
-              favor.
-            </h3>
-            <p className="google-play__text">
-              {/*Prueba la APP. */}Prueba la APP y comienza ahora a cuidar y
-              entrenar tu cuerpo y tu mente
-            </p>
-          </div>
-          <div className="google-play__wrapper-buttons">
-            <div className="google-play__wrapper-download">
-              <a
-                target="_blank"
-                href="https://apps.apple.com/us/app/indigo-balance/id1611795903"
-                className="google-play__button-ios"
-                rel="noreferrer"
-              >
-                <img
-                  className="google-play__button-ios"
-                  src="/img/download-ios.svg"
-                />
-              </a>
-              <a
-                target="_blank"
-                href="https://play.google.com/store/apps/details?id=com.indigobalance.app"
-                className="google-play__button"
-                rel="noreferrer"
-              >
-                <img src="/img/download-google.svg" />
-              </a>
-            </div>
-          </div>
->>>>>>> aee4788c2ecff0effaa5b499c346670d148f8fe9
         </div>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 import Layout from "components/Layout";
 import Hero from "components/Hero";
 import GooglePlay from "components/GooglePlay";
@@ -7,38 +5,23 @@ import Camino from "components/Camino";
 import Contact from "components/Contact";
 import SubHero from "components/SubHero";
 import KnowMore from "components/KnowMore";
-=======
-import Link from "next/link";
 import Layout from "components/Layout";
 import Hero from "components/Hero";
-import Video from "components/Video";
 import GooglePlay from "components/GooglePlay";
 import Camino from "components/Camino";
-import Testimonials from "components/Testimonials";
 import Contact from "components/Contact";
 import SubHero from "components/SubHero";
->>>>>>> aee4788c2ecff0effaa5b499c346670d148f8fe9
+
 
 export default function Home() {
   return (
     <Layout>
       <Hero />
-<<<<<<< HEAD
-      
       <Camino />
       <GooglePlay />
       
       <SubHero />
       <KnowMore />
-
-=======
-      <SubHero />
-      <GooglePlay />
-      <Camino />
-      <GooglePlay />
-      <Testimonials />
-      <Video />
->>>>>>> aee4788c2ecff0effaa5b499c346670d148f8fe9
       <Contact />
     </Layout>
   );
