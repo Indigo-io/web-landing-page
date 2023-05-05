8b265b0d34d7d7effd5f0ee77cd13f95a95a1041
Exequiel Demaio <exequiel.demaio@gmail.com>
2023-05-05 18:13:57 -0300

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ import "../styles/contact.css";
 import "../styles/start-now.css";
 import "../styles/sub-hero.css";
 import "../styles/privacy.css";
+
 import "../styles/wizard.css";
 
 function MyApp({ Component, pageProps }) {
